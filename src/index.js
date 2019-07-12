ca71ef416222f10e0ea2c0a2bf9684343d322717
Zhouchanglong <changlong.zhou@56qq.com>
2019-07-12 10:16:42 +0800

    feat: 测试file-loader对背景图片的处理

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ import girl from './images/720*880_01.png';
 
 const component = () => {
   const oDiv = document.createElement('div');
-  const oImg = document.createElement('img');
+  const oBgImg = document.createElement('div');
+  const oFragment = document.createDocumentFragment();
+  const oImg = new Image();
   // oDiv.innerHTML = _.join(['Hello', 'Joe'], ' ');
   oDiv.innerHTML = `The total of 1, 2, 9, 10 is ${sum(1, 2, 9, 10)}`;
   oDiv.classList.add('color');
   oImg.src = girl;
   oImg.width = 100;
   oImg.height = 100;
-  oDiv.appendChild(oImg);
-  return oDiv;
+  oBgImg.classList.add('bgImg');
+  oFragment.appendChild(oImg);
+  oFragment.appendChild(oBgImg);
+  oFragment.appendChild(oDiv);
+  return oFragment;
 };
 
 document.body.appendChild(component());
